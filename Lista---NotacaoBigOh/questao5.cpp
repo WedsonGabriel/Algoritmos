c1b5a202ed4524bc04d29f338b164e0f83d5fa78
Wedson Gabriel <wedsongabriel163@gmail.com>
2026-04-21 19:23:56 -0300

    Atualizado

--- Lista---NotacaoBigOh/questao5.cpp
+++ Lista---NotacaoBigOh/questao5.cpp
@@ -3,3 +3,37 @@ Escreva um algoritmo que leia um array a de n (2 ≤ n ≤ 109) números inteiro
 par de números cuja soma seja s (1 ≤ s ≤ 2 × 109), também informado na entrada.
 */
 
+#include "questao5.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tamanho, somaDesejada;
+    cin >> tamanho;
+    vector<int> array(tamanho);
+
+    for (int a = 0; a < tamanho; a++) {
+        cin >> array[a];
+    }
+
+    cin >> somaDesejada;
+    bool result = false;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = i + 1; j < tamanho; j++) {
+            if (array[i] + array[j] == somaDesejada) {
+                result = true;
+                break;
+            }
+        }
+        if (result) {
+            break;
+        }
+    }
+    if (result) {
+        cout << "S" << endl;
+    }
+    else {
+        cout << "N" << endl;
+    }
+    return 0;
+}
